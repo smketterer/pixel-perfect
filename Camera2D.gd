39d1bfcf0aa15da2f6ee59d2e966c8519347d50f
Sheldon Ketterer <smketterer@gmail.com>
2020-06-07 23:49:03 -0600

    Some rendering stuff.

--- Camera2D.gd
+++ Camera2D.gd
@@ -1,19 +1,30 @@
 extends Camera2D
 
-export var DESIRED_RESOLUTION = Vector2(320, 180)
-var vp
-var scaling_factor = 1
+var fixed_toggle_point = Vector2(0,0)
+var current_zoom = 1
+var target_zoom = 1
+export var ZOOM_LEVELS = [.25, .5, 1, 1.5, 2]
+export var ZOOM_SPEED = 10
 
-func _ready():
-	vp = get_viewport()
-	vp.connect(
-	"size_changed", self, "on_vp_size_change"
-  )
-	on_vp_size_change()
-
-func on_vp_size_change():
-	var scale_vector = vp.size / DESIRED_RESOLUTION
-	var new_scaling_factor = max(floor(min(scale_vector[0], scale_vector[1])), 1)
-	if new_scaling_factor != scaling_factor:
-		scaling_factor = new_scaling_factor
-		zoom = Vector2(1 / scaling_factor, 1 / scaling_factor)
+func _process(delta):
+	self.global_position.x += (int(Input.is_action_pressed("camera_pan_right")) - int(Input.is_action_pressed("camera_pan_left"))) * 500 * current_zoom * delta
+	self.global_position.y += (int(Input.is_action_pressed("camera_pan_down")) - int(Input.is_action_pressed("camera_pan_up"))) * 500 * current_zoom * delta
+	
+	if Input.is_action_just_pressed("mouse_middle"):
+		var ref = get_viewport().get_mouse_position()
+		fixed_toggle_point = ref
+	if Input.is_action_pressed("mouse_middle"):
+		var ref = get_viewport().get_mouse_position()
+		self.global_position.x -= (ref.x - fixed_toggle_point.x) * current_zoom
+		self.global_position.y -= (ref.y - fixed_toggle_point.y) * current_zoom
+		fixed_toggle_point = ref
+	if Input.is_action_just_released("mouse_scroll_up"):
+		var current_zoom_index = ZOOM_LEVELS.find(target_zoom)
+		if current_zoom_index > 0:
+			target_zoom = ZOOM_LEVELS[current_zoom_index - 1]
+	if Input.is_action_just_released("mouse_scroll_down"):
+		var current_zoom_index = ZOOM_LEVELS.find(target_zoom)
+		if current_zoom_index < ZOOM_LEVELS.size() - 1:
+			target_zoom = ZOOM_LEVELS[current_zoom_index + 1]
+	current_zoom = lerp(current_zoom, target_zoom, ZOOM_SPEED * delta)
+	set_zoom(Vector2(current_zoom,current_zoom))
--- Player.gd
+++ Player.gd
@@ -1,42 +1,28 @@
 extends KinematicBody2D
 
-const TARGET_FPS = 60
-const ACCELERATION = 8
-const MAX_SPEED = 68
-const FRICTION = 15
-const AIR_RESISTANCE = .5
-const GRAVITY = 6
-const JUMP_FORCE = 160
+# Speed at which the sidekick will move
+const MOVEMENT_SPEED = 96
+# How close the sidekick must be to a point before moving on
+const POINT_RADIUS = 5
+var path
+onready var nav = get_node("../TileMap")
 
-var motion = Vector2.ZERO
 
-onready var sprite = $AnimatedSprite
+func _input(event):
+	if Input.is_action_just_pressed("mouse_right"):
+		path = nav.get_astar_path(position, event.position)
+		if path:
+			path.remove(0)
+			print_debug('got path')
 
-func _physics_process(delta):
-	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	motion.y += GRAVITY * delta * TARGET_FPS
-	
-	if x_input != 0:
-		motion.x += x_input * ACCELERATION * delta * TARGET_FPS
-		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
-		sprite.flip_h = x_input < 0
-	
-	if is_on_floor():
-		if x_input != 0:
-			sprite.play("walk")
-		else:
-			sprite.play("stand")
-			motion.x = lerp(motion.x, 0, FRICTION * delta)
-		if Input.is_action_just_pressed("ui_up"):
-			motion.y = -JUMP_FORCE
-	else:
-		if sprite.animation != "jump":
-			sprite.play("jump")
-		
-		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2:
-			motion.y = -JUMP_FORCE/2
-		
-		if x_input == 0:
-			motion.x = lerp(motion.x, 0, AIR_RESISTANCE * delta)
-	
-	motion = move_and_slide(motion, Vector2.UP)
+# Performed on each step
+func _process(delta):
+	if path:
+		var target = path[0]
+		var direction = (target - position).normalized()
+		position += direction * MOVEMENT_SPEED * delta
+		if position.distance_to(target) < POINT_RADIUS:
+			# Remove first path point
+			path.remove(0)
+			if path.size() == 0:
+				path = null
